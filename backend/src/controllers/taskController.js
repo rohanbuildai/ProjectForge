7716690fd33d486459d82a1d1acc950ad15b6781
Rohan Singh <8e44rohansinghkumar@gmail.com>
2026-07-19 15:49:03 +0530

    feat(tasks): implement delete task endpoint

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -311,6 +311,69 @@ const updateTask = async (req, res) => {
     }
 }
 
+
+const deleteTask = async (req, res) => {
+    try {
+
+        const taskId = Number(req.params.taskId);
+        const { id } = req.user;
+
+        if (!Number.isInteger(taskId) || taskId <= 0) {
+            return res.status(400).json({
+                success: false,
+                message: "Enter valid input"
+            })
+        }
+
+        const task = await pool.query(
+            `SELECT project_id FROM tasks
+             WHERE id = $1`, [taskId]
+        )
+
+        if (task.rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "Task does not exist"
+            })
+        }
+
+        const projectId = task.rows[0].project_id;
+
+        const strictlyValidateProject = await pool.query(
+            `SELECT id FROM projects
+             WHERE id = $1
+             AND user_id = $2`, [projectId, id]
+        )
+
+        if (strictlyValidateProject.rows.length === 0) {
+            return res.status(403).json({
+                success: false,
+                message: "You are not authorized to access this project."
+            })
+        }
+
+        const deletedTask = await pool.query(
+            `DELETE FROM tasks
+             WHERE id = $1
+             RETURNING *;`,[taskId]
+        )
+
+        return res.status(200).json({
+            success: true,
+            message: "Task deleted successfully",
+            data: deletedTask.rows[0]
+        });
+    }
+    catch (error) {
+        console.error(error)
+
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }
+}
+
 module.exports = {
     createTask,
     getTasksByProject,
