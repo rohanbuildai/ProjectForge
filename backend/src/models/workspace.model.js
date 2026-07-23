d65f4e4de66c8b7827d9d2b0cba5f825e2637798
Rohan Singh <rohan.buildai@gmail.com>
2026-07-23 15:26:08 +0530

    feat(workspace): implement get user workspaces endpoint

--- backend/src/models/workspace.model.js
+++ backend/src/models/workspace.model.js
@@ -22,6 +22,30 @@ const createWorkspace = async ({ client, name, description, createdBy }) => {
     return result.rows[0];
 };
 
+
+const getUserWorkspaces = async ( { client , userId }) => {
+
+    const query = `
+     SELECT
+        w.id,
+        w.name,
+        w.description,
+        wm.role,
+        w.created_at
+     FROM workspaces w
+     INNER JOIN workspace_members wm
+     ON w.id = wm.workspace_id
+     WHERE wm.user_id = $1 ;
+     `
+
+     const values = [ userId ]
+
+     const result = await client.query ( query , values )
+
+     return result.rows
+}
+
 module.exports = {
-    createWorkspace
+    createWorkspace ,
+    getUserWorkspaces
 } ;
--- backend/src/routes/workspaceRoutes.js
+++ backend/src/routes/workspaceRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
 const { validateCreateWorkspace } = require("../validations/workspace.validation");
-const { createWorkspace } = require("../controllers/workspaceController");
+const { createWorkspace , getWorkspaces } = require("../controllers/workspaceController");
 
 const router = express.Router();
 
-router.post("/",authMiddleware,validateCreateWorkspace,createWorkspace)
+router.post("/" , authMiddleware , validateCreateWorkspace , createWorkspace)
+router.get("/" , authMiddleware , getWorkspaces)
 
 module.exports = router;
